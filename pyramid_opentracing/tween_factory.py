f28c6fae446b12a37947a37032b8c793c0826a42
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2017-02-27 21:08:31 +0100

    Add documenting comments to tween_factory.

--- pyramid_opentracing/tween_factory.py
+++ pyramid_opentracing/tween_factory.py
@@ -3,6 +3,11 @@ import opentracing
 from .tracer import PyramidTracer
 
 def opentracing_tween_factory(handler, registry):
+    '''
+    The factory method is called once, and we thus retrieve the settings as defined
+    on the global configuration.
+    We set the 'opentracing_tracer' in the settings to, for further reference and usage.
+    '''
     base_tracer = registry.settings.get('opentracing_base_tracer', opentracing.Tracer())
     traced_attrs = registry.settings.get('opentracing_traced_attributes', [])
     trace_all = registry.settings.get('opentracing_trace_all', False)
@@ -11,6 +16,8 @@ def opentracing_tween_factory(handler, registry):
     registry.settings ['opentracing_tracer'] = tracer
 
     def opentracing_tween(req):
+        # if tracing for all requests is disabled, continue with the
+        # normal handlers flow and return immediately.
         if not trace_all:
             return handler(req)
 
@@ -23,5 +30,9 @@ def opentracing_tween_factory(handler, registry):
     return opentracing_tween
 
 def includeme(config):
+    '''
+    Set up an implicit 'tween' to do tracing on all the requests, with
+    optionally including fields on the request object (method, url, path, etc).
+    '''
     config.add_tween('pyramid_opentracing.opentracing_tween_factory')
 
