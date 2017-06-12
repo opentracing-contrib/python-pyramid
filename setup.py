5c2356ae75c975ea451cde1dfbeef293488b6a7e
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2017-06-12 18:30:59 +0200

    Update setup.py with the opentracing-contrib repo info.

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ version = open('VERSION').read()
 setup(
     name='pyramid_opentracing',
     version=version,
-    url='https://github.com/carlosalberto/python-pyramid/',
-    download_url='https://github.com/carlosalberto/python-pyramid/tarball/'+version,
+    url='https://github.com/opentracing-contrib/python-pyramid/',
+    download_url='https://github.com/opentracing-contrib/python-pyramid/tarball/'+version,
     license='BSD',
     author='Carlos Alberto Cortez',
     author_email='calberto.cortez@gmail.com',
